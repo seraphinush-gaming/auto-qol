ad226ba471a2c49bc06f41afbf0362b82746a570
seraph <seraphinush@gmail.com>
2019-09-05 14:13:03 +0900

    2.06.02

--- submodules/auto-inspect.js
+++ submodules/auto-inspect.js
@@ -6,16 +6,10 @@ class AutoInspect {
 
     this.parent.hook('S_ANSWER_INTERACTIVE', 2, {}, (e) => {
       if (this.parent.settings.enableInspect) {
-        if (this.parent.mod.majorPatchVersion >= 85) {
-          this.parent.mod.send('C_REQUEST_USER_PAPERDOLL_INFO', 2, {
-            unk: true,
-            name: e.name
-          });
-        } else {
-          this.parent.mod.send('C_REQUEST_USER_PAPERDOLL_INFO', 1, {
-            name: e.name
-          });
-        }
+        this.parent.mod.send('C_REQUEST_USER_PAPERDOLL_INFO', 2, {
+          unk: true,
+          name: e.name
+        });
       }
     });
 
